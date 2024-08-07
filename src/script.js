e69732cc0e3c17de2740480c7b0decdf9f2d4b08
Demi-Ajayi <demiajayi02@gmail.com>
2024-08-07 12:09:16 +0200

    updated javascript to display live time using moment.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
-alert("Hello!");
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".city-time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<span>]A[</span>]"
+  );
+
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgDateElement = johannesburgElement.querySelector(".date");
+  let johannesburgTimeElement = johannesburgElement.querySelector(".city-time");
+  let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+  johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+  johannesburgTimeElement.innerHTML = johannesburgTime.format(
+    "h:mm:ss [<span>]A[</span>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
